d79b39976a9ba6a96591c49a05fa4de698e93861
Nicholas Lourie <nicholasl@allenai.org>
2020-03-11 23:52:14 -0700

    Factor mixing rates into their own module
    Factor mixing rates into their own module and use for loops to
    declare mixtures with different mixing rates.

--- src/rainbow/mixtures.py
+++ src/rainbow/mixtures.py
@@ -2,7 +2,9 @@
 
 import t5
 
-from . import core, datasets, settings, tasks
+from . import datasets, rates, settings
+
+import rainbow.tasks
 
 # N.B. tasks must be imported before mixtures, so that the mixtures can use
 # the tasks in their definitions.
@@ -32,72 +34,47 @@ for dataset in datasets.KNOWLEDGE_GRAPH_DATASETS.values():
             )
 
 # Create the Rainbow mixtures.
-t5.data.MixtureRegistry.add(
-    "rainbow_equal_mixture",
-    [f"{dataset.name}_task" for dataset in datasets.RAINBOW_DATASETS.values()],
-    default_rate=core.equal_rate,
-)
-
-t5.data.MixtureRegistry.add(
-    "rainbow_proportional_mixture",
-    [f"{dataset.name}_task" for dataset in datasets.RAINBOW_DATASETS.values()],
-    default_rate=core.proportional_rate,
-)
+for rate_name, rate_func in rates.MIXING_RATES.items():
+    t5.data.MixtureRegistry.add(
+        f"rainbow_{rate_name}_mixture",
+        [
+            f"{dataset.name}_task"
+            for dataset in datasets.RAINBOW_DATASETS.values()
+        ],
+        default_rate=rate_func,
+    )
 
 # Create mixtures with rainbow and a single knowledge graph.
 for dataset in datasets.KNOWLEDGE_GRAPH_DATASETS.values():
     for direction in settings.KNOWLEDGE_GRAPH_DIRECTIONS:
-        t5.data.MixtureRegistry.add(
-            f"rainbow_{dataset.name}_{direction}_equal_mixture",
-            [f"{dataset.name}_{direction}_task"]
-            + [
-                f"{rainbow_dataset.name}_task"
-                for rainbow_dataset in datasets.RAINBOW_DATASETS.values()
-            ],
-            default_rate=core.equal_rate,
-        )
-
-        t5.data.MixtureRegistry.add(
-            f"rainbow_{dataset.name}_{direction}_proportional_mixture",
-            [f"{dataset.name}_{direction}_task"]
-            + [
-                f"{rainbow_dataset.name}_task"
-                for rainbow_dataset in datasets.RAINBOW_DATASETS.values()
-            ],
-            default_rate=core.proportional_rate,
-        )
+        for rate_name, rate_func in rates.MIXING_RATES.items():
+            t5.data.MixtureRegistry.add(
+                f"rainbow_{dataset.name}_{direction}_{rate_name}_mixture",
+                [f"{dataset.name}_{direction}_task"]
+                + [
+                    f"{rainbow_dataset.name}_task"
+                    for rainbow_dataset in datasets.RAINBOW_DATASETS.values()
+                ],
+                default_rate=rate_func,
+            )
 
 # Create mixtures with rainbow and all knowledge graphs.
 for direction in settings.KNOWLEDGE_GRAPH_DIRECTIONS:
-    t5.data.MixtureRegistry.add(
-        f"rainbow_comet_{direction}_equal_mixture",
-        [
-            # include the knowledge graphs
-            f"{dataset.name}_{direction}_task"
-            for dataset in datasets.KNOWLEDGE_GRAPH_DATASETS.values()
-        ]
-        + [
-            # include the rainbow datasets
-            f"{dataset.name}_task"
-            for dataset in datasets.RAINBOW_DATASETS.values()
-        ],
-        default_rate=core.equal_rate,
-    )
-
-    t5.data.MixtureRegistry.add(
-        f"rainbow_comet_{direction}_proportional_mixture",
-        [
-            # include the knowledge graphs
-            f"{dataset.name}_{direction}_task"
-            for dataset in datasets.KNOWLEDGE_GRAPH_DATASETS.values()
-        ]
-        + [
-            # include the rainbow datasets
-            f"{dataset.name}_task"
-            for dataset in datasets.RAINBOW_DATASETS.values()
-        ],
-        default_rate=core.proportional_rate,
-    )
+    for rate_name, rate_func in rates.MIXING_RATES.items():
+        t5.data.MixtureRegistry.add(
+            f"rainbow_comet_{direction}_{rate_name}_mixture",
+            [
+                # include the knowledge graphs
+                f"{dataset.name}_{direction}_task"
+                for dataset in datasets.KNOWLEDGE_GRAPH_DATASETS.values()
+            ]
+            + [
+                # include the rainbow datasets
+                f"{dataset.name}_task"
+                for dataset in datasets.RAINBOW_DATASETS.values()
+            ],
+            default_rate=rate_func,
+        )
 
 # Create the Rainbow multi-tasking learning curve mixtures.
 for dataset in datasets.RAINBOW_DATASETS.values():
@@ -105,27 +82,17 @@ for dataset in datasets.RAINBOW_DATASETS.values():
         if size is None:
             continue
 
-        t5.data.MixtureRegistry.add(
-            f"{dataset.name}_{size:05}_rainbow_equal_mixture",
-            [f"{dataset.name}_{size:05}_task"]
-            + [
-                f"{other_dataset.name}_task"
-                for other_dataset in datasets.RAINBOW_DATASETS.values()
-                if other_dataset != dataset
-            ],
-            default_rate=core.equal_rate,
-        )
-
-        t5.data.MixtureRegistry.add(
-            f"{dataset.name}_{size:05}_rainbow_proportional_mixture",
-            [f"{dataset.name}_{size:05}_task"]
-            + [
-                f"{other_dataset.name}_task"
-                for other_dataset in datasets.RAINBOW_DATASETS.values()
-                if other_dataset != dataset
-            ],
-            default_rate=core.proportional_rate,
-        )
+        for rate_name, rate_func in rates.MIXING_RATES.items():
+            t5.data.MixtureRegistry.add(
+                f"{dataset.name}_{size:05}_rainbow_{rate_name}_mixture",
+                [f"{dataset.name}_{size:05}_task"]
+                + [
+                    f"{other_dataset.name}_task"
+                    for other_dataset in datasets.RAINBOW_DATASETS.values()
+                    if other_dataset != dataset
+                ],
+                default_rate=rate_func,
+            )
 
 # Create the GLUE multi-tasking learning curve mixtures.
 for dataset in datasets.RAINBOW_DATASETS.values():
@@ -133,25 +100,16 @@ for dataset in datasets.RAINBOW_DATASETS.values():
         if size is None:
             continue
 
-        t5.data.MixtureRegistry.add(
-            f"{dataset.name}_{size:05}_glue_equal_mixture",
-            [f"{dataset.name}_{size:05}_task"]
-            + [
-                f"glue_{glue_dataset.name}_v002"
-                for glue_dataset in datasets.GLUE_DATASETS.values()
-            ],
-            default_rate=core.equal_rate,
-        )
-
-        t5.data.MixtureRegistry.add(
-            f"{dataset.name}_{size:05}_glue_proportional_mixture",
-            [f"{dataset.name}_{size:05}_task"]
-            + [
-                f"glue_{glue_dataset.name}_v002"
-                for glue_dataset in datasets.GLUE_DATASETS.values()
-            ],
-            default_rate=core.proportional_rate,
-        )
+        for rate_name, rate_func in rates.MIXING_RATES.items():
+            t5.data.MixtureRegistry.add(
+                f"{dataset.name}_{size:05}_glue_{rate_name}_mixture",
+                [f"{dataset.name}_{size:05}_task"]
+                + [
+                    f"glue_{glue_dataset.name}_v002"
+                    for glue_dataset in datasets.GLUE_DATASETS.values()
+                ],
+                default_rate=rate_func,
+            )
 
 # Create the Super GLUE multi-tasking learning curve mixtures.
 for dataset in datasets.RAINBOW_DATASETS.values():
@@ -159,22 +117,13 @@ for dataset in datasets.RAINBOW_DATASETS.values():
         if size is None:
             continue
 
-        t5.data.MixtureRegistry.add(
-            f"{dataset.name}_{size:05}_super_glue_equal_mixture",
-            [f"{dataset.name}_{size:05}_task"]
-            + [
-                f"super_glue_{super_glue_dataset.name}_v102"
-                for super_glue_dataset in datasets.SUPER_GLUE_DATASETS.values()
-            ],
-            default_rate=core.equal_rate,
-        )
-
-        t5.data.MixtureRegistry.add(
-            f"{dataset.name}_{size:05}_super_glue_proportional_mixture",
-            [f"{dataset.name}_{size:05}_task"]
-            + [
-                f"super_glue_{super_glue_dataset.name}_v102"
-                for super_glue_dataset in datasets.SUPER_GLUE_DATASETS.values()
-            ],
-            default_rate=core.proportional_rate,
-        )
+        for rate_name, rate_func in rates.MIXING_RATES.items():
+            t5.data.MixtureRegistry.add(
+                f"{dataset.name}_{size:05}_super_glue_{rate_name}_mixture",
+                [f"{dataset.name}_{size:05}_task"]
+                + [
+                    f"super_glue_{super_glue_dataset.name}_v102"
+                    for super_glue_dataset in datasets.SUPER_GLUE_DATASETS.values()
+                ],
+                default_rate=rate_func,
+            )
--- tests/rainbow/test_core.py
+++ tests/rainbow/test_core.py
@@ -8,31 +8,8 @@ import unittest
 import t5
 import tensorflow as tf
 
-from rainbow import core, datasets, preprocessors, tasks
+from rainbow import core, preprocessors
 from .. import settings, utils
-
-# N.B. we must import tasks so that the tasks are registered and
-# available for running tests.
-
-
-class ProportionalRateTestCase(unittest.TestCase):
-    """Tests for rainbow.core.proportional_rate."""
-
-    def test_returns_training_set_size(self):
-        for dataset in datasets.RAINBOW_DATASETS.values():
-            task = t5.data.get_mixture_or_task(f"{dataset.name}_task")
-            self.assertEqual(
-                core.proportional_rate(task), dataset.splits["train"].size
-            )
-
-
-class EqualRateTestCase(unittest.TestCase):
-    """Tests for rainbow.core.equal_rate."""
-
-    def test_returns_one(self):
-        for dataset in datasets.RAINBOW_DATASETS.values():
-            task = t5.data.get_mixture_or_task(f"{dataset.name}_task")
-            self.assertEqual(core.equal_rate(task), 1.0)
 
 
 class CsvTaskTestCase(unittest.TestCase):
